676ee96a19f3c735f6e81d3a93b6d3c36f548c87
Andru Schurov <a.schurov@l-tech.ru>
2018-12-18 12:54:12 +0300

    add amout to capture

--- spec/yandex_kassa_spec.rb
+++ spec/yandex_kassa_spec.rb
@@ -3,7 +3,7 @@ RSpec.describe YandexKassa do
     expect(YandexKassa::VERSION).not_to be nil
   end
 
-  it "does something useful" do
+  xit "does something useful" do
     expect(false).to eq(true)
   end
 end
