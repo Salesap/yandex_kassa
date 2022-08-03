f9c6dcdaaf18a645683a34659ed90f4c9bebe3f2
Andrey Yakovlev <sigra.yandex@gmail.com>
2022-08-03 11:58:16 +0300

    Change payment gateway API to Yoomoney
    `payment.yandex.net` is not available now

--- lib/yandex_kassa/api_struct_settings.rb
+++ lib/yandex_kassa/api_struct_settings.rb
@@ -1,7 +1,7 @@
 ApiStruct::Settings.configure do |config|
   config.endpoints = {
     yandex_kassa: {
-      root: 'https://payment.yandex.net/api/v3',
+      root: 'https://api.yookassa.ru/v3',
       headers: {
         'Content-Type': 'application/json'
       }
